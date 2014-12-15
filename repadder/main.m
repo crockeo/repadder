df2e2fb5c103956528ce95debd5fd36d0e57753a
Cerek Hillen <crockeo@gmail.com>
2014-12-15 16:32:18 -0500

    Initial Commit

---
+++ repadder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  repadder
+//
+//  Created by Cerek Hillen on 12/15/14.
+//  Copyright (c) 2014 Cerek Hillen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
