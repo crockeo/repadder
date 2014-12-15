3eb04fed3db7b5e88b9edb010d6c1c0f0825d02c
Cerek Hillen <crockeo@gmail.com>
2014-12-14 22:26:50 -0500

    Ironed out some controller stuff.

--- src/controller.cpp
+++ src/controller.cpp
@@ -10,6 +10,10 @@
 // Attempting to construct a controller at a given index.
 Controller::Controller(int index) {
     this->joy = SDL_JoystickOpen(index);
+    if (this->joy == nullptr)
+        this->error = true;
+    else
+        this->error = false;
 }
 
 // Cleaning up a controller.
@@ -20,30 +24,31 @@ Controller::~Controller() {
 
 // Getting the number of axes.
 int Controller::getNumAxes() {
-    if (!this->isError())
-        SDL_JoystickNumAxes(this->joy);
-    return -1;
+    if (this->isError())
+        return -1;
+    return SDL_JoystickNumAxes(this->joy);
 }
 
 // Getting the number of buttons.
 int Controller::getNumButtons() {
-    if (!this->isError())
-        SDL_JoystickNumButtons(this->joy);
-    return -1;
+    if (this->isError())
+        return -1;
+
+    return SDL_JoystickNumButtons(this->joy);
 }
 
 // Getting the value of an axis.
 Sint16 Controller::getAxis(int index) {
-    if (!this->isError())
-        return SDL_JoystickGetAxis(this->joy, index);
-    return 0;
+    if (this->isError())
+        return 0;
+    return SDL_JoystickGetAxis(this->joy, index);
 }
 
 // Checking the state of a button.
 bool Controller::getButton(int index) {
-    if (!this->isError())
-        return SDL_JoystickGetButton(this->joy, index) == 1;
-    return false;
+    if (this->isError())
+        return false;
+    return SDL_JoystickGetButton(this->joy, index) == 1;
 }
 
 // Checking if there was an error in creating a controller.
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+#include "controller.hpp"
+
 //////////
 // Code //
 
@@ -10,19 +12,29 @@
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Joystick* joy;
-    for (int i = 0; i < SDL_NumJoysticks() > 0; i++) {
-        joy = SDL_JoystickOpen(i);
+    Controller* c = new Controller(0);
+    if (c->isError())
+        return 1;
 
-        if (joy) {
-            std::cout << "Opened joystick #" << i << ": " << SDL_JoystickNameForIndex(i) << ".\n";
-            std::cout << "Number of Axes #" << i << ": " << SDL_JoystickNumAxes(joy) << ".\n";
-            std::cout << "Number of Buttons #" << i << ": " << SDL_JoystickNumButtons(joy) << ".\n";
-            std::cout << "Number of Balls #" << i << ": " << SDL_JoystickNumBalls(joy) << ".\n";
+    std::cout << c->getNumAxes() << "\n";
+    std::cout << c->getNumButtons() << "\n";
 
-            if (SDL_JoystickGetAttached(joy))
-                SDL_JoystickClose(joy);
-        } else
-            std::cout << "Couldn't open the joystick " << i << ".\n";
-    }
+    delete c;
+    return 0;
+
+    //SDL_Joystick* joy;
+    //for (int i = 0; i < SDL_NumJoysticks() > 0; i++) {
+        //joy = SDL_JoystickOpen(i);
+
+        //if (joy) {
+            //std::cout << "Opened joystick #" << i << ": " << SDL_JoystickNameForIndex(i) << ".\n";
+            //std::cout << "Number of Axes #" << i << ": " << SDL_JoystickNumAxes(joy) << ".\n";
+            //std::cout << "Number of Buttons #" << i << ": " << SDL_JoystickNumButtons(joy) << ".\n";
+            //std::cout << "Number of Balls #" << i << ": " << SDL_JoystickNumBalls(joy) << ".\n";
+
+            //if (SDL_JoystickGetAttached(joy))
+                //SDL_JoystickClose(joy);
+        //} else
+            //std::cout << "Couldn't open the joystick " << i << ".\n";
+    //}
 }
