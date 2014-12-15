f39b89e736aeab1f62c735969c6d107f269a542a
Cerek Hillen <crockeo@gmail.com>
2014-12-14 22:12:30 -0500

    Added a bit of stuff to deal with exploring the joystick.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 //////////////
 // Includes //
+#include <SDL2/SDL.h>
 #include <iostream>
 
 //////////
@@ -7,5 +8,21 @@
 
 // The entry point to the application.
 int main() {
-    std::cout << "Hello world!\n";
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Joystick* joy;
+    for (int i = 0; i < SDL_NumJoysticks() > 0; i++) {
+        joy = SDL_JoystickOpen(i);
+
+        if (joy) {
+            std::cout << "Opened joystick #" << i << ": " << SDL_JoystickNameForIndex(i) << ".\n";
+            std::cout << "Number of Axes #" << i << ": " << SDL_JoystickNumAxes(joy) << ".\n";
+            std::cout << "Number of Buttons #" << i << ": " << SDL_JoystickNumButtons(joy) << ".\n";
+            std::cout << "Number of Balls #" << i << ": " << SDL_JoystickNumBalls(joy) << ".\n";
+
+            if (SDL_JoystickGetAttached(joy))
+                SDL_JoystickClose(joy);
+        } else
+            std::cout << "Couldn't open the joystick " << i << ".\n";
+    }
 }
