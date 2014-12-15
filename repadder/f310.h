e00562c41828564fb1729a81ff659c44e8c2a889
Cerek Hillen <crockeo@gmail.com>
2014-12-15 17:15:27 -0500

    EVEN MORE DOCS

--- repadder/f310.h
+++ repadder/f310.h
@@ -24,4 +24,36 @@ const Uint8 F310_START  = 9;
 const Uint8 F310_LEFT_STICK  = 10;
 const Uint8 F310_RIGHT_STICK = 11;
 
+// The axes.
+const Uint8  F310_LEFT_STICK_X     = 0;
+const SInt16 F310_LEFT_STICK_X_MIN = -32768;
+const SInt16 F310_LEFT_STICK_X_MAX =  32767;
+
+const Uint8  F310_LEFT_STICK_Y     = 0;
+const SInt16 F310_LEFT_STICK_Y_MIN = -32768;
+const SInt16 F310_LEFT_STICK_Y_MAX =  32767;
+
+const Uint8  F310_RIGHT_STICK_X     = 0;
+const SInt16 F310_RIGHT_STICK_X_MIN = -32768;
+const SInt16 F310_RIGHT_STICK_X_MAX =  32767;
+
+const Uint8  F310_RIGHT_STICK_Y     = 0;
+const SInt16 F310_RIGHT_STICK_Y_MIN = -32768;
+const SInt16 F310_RIGHT_STICK_Y_MAX =  32767;
+
+// The different values for the hat (DPad)
+const Uint8 F310_DPAD = 0;
+
+const Uint8 F310_DPAD_NEUTRAL = 0;
+const Uint8 F310_DPAD_UP      = 1;
+const Uint8 F310_DPAD_RIGHT   = 2;
+const Uint8 F310_DPAD_DOWN    = 4;
+const Uint8 F310_DPAD_LEFT    = 8;
+
+const UInt8 F310_DPAD_UP_RIGHT   = F310_DPAD_UP   + F310_DPAD_RIGHT;
+const UInt8 F310_DPAD_DOWN_RIGHT = F310_DPAD_DOWN + F310_DPAD_RIGHT;
+
+const UInt8 F310_DPAD_UP_LEFT   = F310_DPAD_UP   + F310_DPAD_LEFT;
+const UInt8 F310_DPAD_DOWN_LEFT = F310_DPAD_DOWN + F310_DPAD_LEFT;
+
 #endif
