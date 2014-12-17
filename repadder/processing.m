d8a1444b432a73d5ec201b5c6d81297a60d76841
Cerek Hillen <crockeo@gmail.com>
2014-12-16 22:21:52 -0500

    Made the processing actually deal with the config.

--- repadder/processing.m
+++ repadder/processing.m
@@ -4,20 +4,62 @@
 // Includes //
 #import <SDL2/SDL.h>
 
+#import "keyboard.h"
 #import "config.h"
 
 //////////
 // Code //
 
+// Processing an SDL_JoyButtonEvent.
+int processJoyButtonEvent(Config* cfg, SDL_JoyButtonEvent e) {
+    ButtonMap bm;
+    for (int i = 0; i < cfg.buttonMapCount; i++) {
+        bm = cfg.buttonMaps[i];
+        if (bm.joy == e.which && bm.button == e.button) {
+            writeCharacter(e.state == 1, bm.target);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+// Processing an SDL_JoyAxisEvent.
+int processJoyAxisEvent(Config* cfg, SDL_JoyAxisEvent e) {
+    JoystickMap jm;
+    for (int i = 0; i < cfg.joystickMapCount; i++) {
+        jm = cfg.joystickMaps[i];
+        if (jm.joy == e.which && jm.joystick == e.axis) {
+            writeCharacter(jm.min <= e.value && e.value <= jm.max, jm.target);
+            break;
+        }
+    }
+
+    return 0;
+}
+
+// Processing an SDL_JoyHatEvent.
+int processJoyHatEvent(Config* cfg, SDL_JoyHatEvent e) {
+    HatMap hm;
+    for (int i = 0; i < cfg.hatMapCount; i++) {
+        hm = cfg.hatMaps[i];
+        if (hm.joy == e.which && hm.hat == e.hat) {
+            writeCharacter(hm.value == e.value, hm.target);
+            break;
+        }
+    }
+
+    return 0;
+}
+
 // Processing a single joystick event.
 int processJoystickEvent(Config* cfg, SDL_Event e) {
-    if (e.type == SDL_JOYBUTTONUP || e.type == SDL_JOYBUTTONDOWN) {
-        // TODO: Do something.
-    } else if (e.type == SDL_JOYAXISMOTION) {
-        // TODO: Do something.
-    } else if (e.type == SDL_JOYHATMOTION) {
-        // TODO: Do something.
-    }
+    if (e.type == SDL_JOYBUTTONUP || e.type == SDL_JOYBUTTONDOWN)
+        return processJoyButtonEvent(cfg, e.jbutton);
+    else if (e.type == SDL_JOYAXISMOTION)
+        return processJoyAxisEvent(cfg, e.jaxis);
+    else if (e.type == SDL_JOYHATMOTION)
+        return processJoyHatEvent(cfg, e.jhat);
 
     return 0;
 }
