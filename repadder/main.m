92990fc968ea3304f50ec94263c634daeaf94a82
Cerek Hillen <crockeo@gmail.com>
2014-12-16 21:27:27 -0500

    Added logging for the state of the program when it starts.

--- repadder/main.m
+++ repadder/main.m
@@ -24,26 +24,40 @@ int main(int argc, const char** argv) {
     Config* cfg = [Config alloc];
     if (loadConfig(argv[1], cfg) != 0)
         return 1;
+    printf("Loaded config file.\n");
 
     // Initializing SDL.
     if (SDL_Init(SDL_INIT_JOYSTICK) != 0) {
         printf("Failed to initialize SDL.\n");
         return 1;
     }
+    printf("Initialized SDL.\n");
+
+    // Getting the number of joysticks.
+    int numJoys = SDL_NumJoysticks();
+    if (numJoys < 1) {
+        printf("Not enough joysticks.\n");
+        return 1;
+    }
 
     // Opening all of the joysticks.
-    SDL_Joystick** joys = news(SDL_Joystick*, [cfg joysticksNum]);
-    for (int i = 0; i < [cfg joysticksNum]; i++) {
-        joys[i] = SDL_JoystickOpen([cfg joysticks][i]);
+    SDL_Joystick** joys = news(SDL_Joystick*, numJoys);
+    for (int i = 0; i < numJoys; i++) {
+        joys[i] = SDL_JoystickOpen(i);
+
         if (joys[i] == NULL) {
-            printf("Failed to open Joystick #%d!\n", [cfg joysticks][i]);
+            printf("Failed to open joystick #%d.\n", i);
             return 1;
         }
     }
+    printf("Loaded joysticks.\n");
 
     // Actually processing the event.
+    printf("Listening for joystick events...\n");
     processEventQueue(cfg);
 
     // Exiting with success.
+    for (int i = 0; i < numJoys; i++)
+        SDL_JoystickClose(joys[i]);
     return 0;
 }
